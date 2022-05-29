5f732e201dbff740dc1f319507fd347b578fe6f7
LazyAsSleepingCat <LazyAsSleepingCat@users.noreply.github.com>
2022-05-30 00:03:54 +0200

    Add login and authorization

--- stonks/search/urls.py
+++ stonks/search/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('register/', views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='search/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='search/logout.html'), name='logout'),
     path('liked/', views.liked, name='liked')
 ]
--- stonks/search/views.py
+++ stonks/search/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 from .models import Like
 from .forms import UserRegisterForm
@@ -8,22 +9,23 @@ from .forms import UserRegisterForm
 def index(request):
     return render(request, 'search/index.html', {})
 
-def liked(request):
-    context = {
-        'objects': Like.objects.all()
-    }
-    return render(request, 'search/user_likes.html', context)
-
-def login(request):
-    return
 
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            messages.success(request, f'Account created successfully!')
-            return redirect('index')
+            messages.success(request, f'Account has been created!')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'search/register.html', {'form': form})
+
+
+@login_required
+def liked(request):
+    context = {
+        'objects': Like.objects.filter(author=request.user.pk)
+    }
+    return render(request, 'search/user_likes.html', context)
+
