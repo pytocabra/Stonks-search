63c8da3834145b1772d9b0d06c72ad84943647a9
MarcinGorcz <gorczyca@student.agh.edu.pl>
2022-06-04 19:17:42 +0200

    yfinance for detail

--- stonks/search/views.py
+++ stonks/search/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .models import Like,StockTickerData
+from .models import Like, StockTickerData
 from .forms import UserRegisterForm
 
 from .update_ticker_database import update_tickers_in_database, clear_table
+from .yafi_data import get_data_for_ticker
 
 def index(request):
     """The main view with a search bar."""
@@ -25,10 +26,12 @@ def index(request):
 def detail(request, ticker):
     """The detail view with a chart and news."""
     # TODO: logic to handle yahoo api and pass data to context
-    context = {
-        'ticker': ticker,
-    }
-    return render(request, 'search/detail.html', context)
+
+    data_for_year, data_for_quarter, data_for_month, news = get_data_for_ticker(ticker)
+    # context = {
+    #     'ticker': ticker,
+    # }
+    return render(request, 'search/detail.html', data_for_year, data_for_quarter, data_for_month, news)
 
 
 def register(request):
