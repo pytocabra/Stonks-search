aa0283c2a4a6c737b78463bdd57b18abe7a3434f
MarcinGorcz <mtsgorczyca@icloud.com>
2022-06-04 08:51:37 +0200

    StockSymbol

--- stonks/search/admin.py
+++ stonks/search/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Like
+from .models import Like, StockData
 
 admin.site.register(Like)
+admin.site.register(StockData)
--- stonks/search/models.py
+++ stonks/search/models.py
@@ -11,3 +11,16 @@ class Like(models.Model):
 
     def __str__(self):
         return str(self.ticker) + ' ' + str(self.date_liked)
+
+
+class StockTickerData(models.Model):
+    symbol = models.CharField(max_length=20)
+    shortName = models.CharField(max_length=20)
+    longName = models.CharField(max_length=100)
+    exchange = models.CharField(max_length=20)
+    market = models.CharField(max_length=20)
+    quoteType = models.CharField(max_length=20)
+    updateDate = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str("Ticker {}, on market {}.".format(self.symbol, self.market))
