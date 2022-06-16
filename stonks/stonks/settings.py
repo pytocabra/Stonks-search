d1db871dc9e6bb461ee7ac5d479f6f049fa9779a
LazyAsSleepingCat <LazyAsSleepingCat@users.noreply.github.com>
2022-06-16 16:41:16 +0200

    deploy

--- stonks/stonks/settings.py
+++ stonks/stonks/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yc%dbhzpwf5h*foz8$y#tsq01ezudg^r5p^$fz1+56!w&qm0ba'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'stormy-meadow-39808.herokuapp.com'
